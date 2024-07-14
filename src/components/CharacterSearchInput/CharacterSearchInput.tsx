042c6b21947a316a513d86a71524a3537ab878f3
sobbingtofu <kukubasak@naver.com>
2024-07-15 00:56:48 +0900

    style: 사용하지 않는 import 제거

--- src/components/CharacterSearchInput/CharacterSearchInput.tsx
+++ src/components/CharacterSearchInput/CharacterSearchInput.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { ChangeEvent, ChangeEventHandler, useCallback } from "react"
+import { ChangeEvent, useCallback } from "react"
 import zustandStore from "@/zustand/zustandStore"
 import { debounce } from "lodash"
 
