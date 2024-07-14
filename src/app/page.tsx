2142188d7dd36bb4433fbb671d03f9160a248c3b
sobbingtofu <kukubasak@naver.com>
2024-07-14 22:07:29 +0900

    style: 메인페이지 배경이미지 변경

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,14 +5,19 @@ import Page from "@/components/Page"
 
 export default function Home() {
   return (
-    <Page>
-      <h1 className="mb-1 text-3xl font-black">해리포터 영화 시리즈</h1>
-      <MovieList />
-      <h1 className="mb-10 mt-8 text-3xl font-black">
-        해리포터의 인물들을 만나보세요
-      </h1>
-      <CharacterSearchInput />
-      <CharacterList />
-    </Page>
+    <main
+      className="h-full w-full bg-[length:100%_auto] bg-top bg-no-repeat py-20"
+      style={{ backgroundImage: 'url("/bgimage.png")' }}
+    >
+      <Page>
+        <h1 className="mb-1 text-3xl font-black">해리포터 영화 시리즈</h1>
+        <MovieList />
+        <h1 className="mb-10 mt-8 text-3xl font-black">
+          해리포터의 인물들을 만나보세요
+        </h1>
+        <CharacterSearchInput />
+        <CharacterList />
+      </Page>
+    </main>
   )
 }
