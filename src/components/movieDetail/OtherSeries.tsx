a1bad169f3c48176aaadcb1f8262c6cf63b9c770
Cameron <lyle.usn@gmail.com>
2024-07-15 08:35:05 +0900

    update: 중복 로직 제거

--- src/components/movieDetail/OtherSeries.tsx
+++ src/components/movieDetail/OtherSeries.tsx
@@ -12,39 +12,34 @@ const OtherSeries: React.FC<OtherSeriesProps> = ({ serial, allMoviesInfo }) => {
   if (allMoviesInfo.length === 0) {
     return
   }
-  const otherMovies = allMoviesInfo
-    .filter((movie) => movie.serial !== serial)
-    .map((movie) => ({
-      serial: movie.serial,
-      title: movie.title,
-      poster: movie.poster,
-    }))
 
   return (
     <div className="relative mb-[35rem] mt-32 text-white">
       <div className="absolute left-[-7rem]">
         <h3 className="mb-4 ml-12 text-2xl font-bold">다른 시리즈 보기</h3>
         <ul className="flex">
-          {otherMovies.map((movie) => {
-            return (
-              <li
-                key={movie.serial}
-                className="h-[15rem] w-full duration-200 ease-in-out hover:scale-110"
-              >
-                <Link href={`/movies/${movie.serial}`}>
-                  <div className="m-1 flex h-full w-[10rem] flex-col items-center">
-                    <Image
-                      src={movie.poster}
-                      alt={`${movie.serial}serial-posterImg`}
-                      width={240}
-                      height={300}
-                    />
-                    <span className="ml-2 text-sm">{movie.title}</span>
-                  </div>
-                </Link>
-              </li>
-            )
-          })}
+          {allMoviesInfo
+            .filter((movie) => movie.serial !== serial)
+            .map((movie) => {
+              return (
+                <li
+                  key={movie.serial}
+                  className="h-[15rem] w-full duration-200 ease-in-out hover:scale-110"
+                >
+                  <Link href={`/movies/${movie.serial}`}>
+                    <div className="m-1 flex h-full w-[10rem] flex-col items-center">
+                      <Image
+                        src={movie.poster}
+                        alt={`${movie.serial}serial-posterImg`}
+                        width={240}
+                        height={300}
+                      />
+                      <span className="ml-2 text-sm">{movie.title}</span>
+                    </div>
+                  </Link>
+                </li>
+              )
+            })}
         </ul>
       </div>
     </div>
